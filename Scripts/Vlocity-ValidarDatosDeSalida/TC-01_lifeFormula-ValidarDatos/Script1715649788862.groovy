aaa488ec5302ed495d60dd0b9044a0ddeff622fa
RobertoMVUltra <jrmendozav@desarrollo-ultrasist.com.mx>
2024-05-13 22:18:24 -0600

    Se agrega la validación del campo isOpcional
    Se agrega en la validación el campo isopcional del response

--- Scripts/Vlocity-ValidarDatosDeSalida/TC-01_lifeFormula-ValidarDatos/Script1715649788862.groovy
+++ Scripts/Vlocity-ValidarDatosDeSalida/TC-01_lifeFormula-ValidarDatos/Script1715649788862.groovy
@@ -20,7 +20,15 @@ import groovy.json.JsonSlurper
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
+/**Es este CP validarenmos los datos de salida 
+ * de las apis vs fecha técnica de acuerdo a las 
+ * reglas:
+ * -Fumador
+ * -Tipo genero
+**/
+
 def jsonSlurper = new JsonSlurper()
+
 
 ResponseObject responseGetPackages = WS.sendRequest(findTestObject('Postman (1)/SIS Banca Seguros/GetPackages LOB/BanInsGetPackagesIL', 
         [('type') : type, ('insuredAge') : insuredAge, ('smoke') : smoke, ('gender') : gender, ('insuredAmount') : insuredAmount]))
@@ -31,20 +39,23 @@ def getPackages = jsonSlurper.parseText(responseGetPackages.getResponseText())
 
 int edad = Integer.parseInt(insuredAge)
 
-if (((gender == 'F') && (smoke == 'false')) && (edad < 23)) {
+if ((edad < 18) || edad>70) {
     String request = getPackages
 
     WS.verifyEqual('{}', request) //validar datos de la respuesta de la  api 
     ////COberturas/////
-} else if (((gender == 'F') && (smoke == 'true')) && (edad < 21)) {
-    String request = getPackages
-
-    WS.verifyEqual('{}', request)
-} else if (((gender == 'M') && (smoke == 'true')) && (edad < 21)) {
-    String request = getPackages
-
-    WS.verifyEqual('{}', request)
-} else {
+} 
+//else if (((gender == 'F') && (smoke == 'true')) && (edad < 21)) {
+//    String request = getPackages
+//
+//    WS.verifyEqual('{}', request)
+//} else if (((gender == 'M') && (smoke == 'true')) && (edad < 21)) {
+//    String request = getPackages
+//
+//    WS.verifyEqual('{}', request)
+//}
+ else {
+	//Datos Principales so producto
     WS.verifyMatch(name_Producto, getPackages.package.name, false, FailureHandling.CONTINUE_ON_FAILURE)
 
     WS.verifyMatch(branchCode, getPackages.package.branchCode, false, FailureHandling.CONTINUE_ON_FAILURE)
@@ -63,63 +74,86 @@ if (((gender == 'F') && (smoke == 'false')) && (edad < 23)) {
 
     CustomKeywords.'packages.ValidarMontosNumericos.validarMonto'(expense, getPackages.package.expense)
 
+	//Cobertura1
     WS.verifyMatch(name0, getPackages.package.coverages[0].name, false, FailureHandling.CONTINUE_ON_FAILURE)
 
-    WS.verifyMatch(banorteCode0, getPackages.package.coverages[0].banorteCode, false, FailureHandling.CONTINUE_ON_FAILURE)
+	WS.verifyMatch(isOptional0, getPackages.package.coverages[0].isOptional.toString(), false, FailureHandling.CONTINUE_ON_FAILURE)
+	
+	WS.verifyMatch(banorteCode0, getPackages.package.coverages[0].banorteCode, false, FailureHandling.CONTINUE_ON_FAILURE)
 
     CustomKeywords.'packages.ValidarMontosNumericos.validarMonto'(price0, getPackages.package.coverages[0].price)
-
+	
+	//Cobertura2
     WS.verifyMatch(name1, getPackages.package.coverages[1].name, false, FailureHandling.CONTINUE_ON_FAILURE)
 
-    WS.verifyMatch(banorteCode1, getPackages.package.coverages[1].banorteCode, false, FailureHandling.CONTINUE_ON_FAILURE)
+	WS.verifyMatch(isOptional1, getPackages.package.coverages[1].isOptional.toString(), false, FailureHandling.CONTINUE_ON_FAILURE)
+	
+	WS.verifyMatch(banorteCode1, getPackages.package.coverages[1].banorteCode, false, FailureHandling.CONTINUE_ON_FAILURE)
 
     CustomKeywords.'packages.ValidarMontosNumericos.validarMonto'(price1, getPackages.package.coverages[1].price)
 
     CustomKeywords.'packages.ValidarMontosNumericos.validarMonto'(insuredAmount1, getPackages.package.coverages[1].insuredAmount)
-
+	
+	//Cobertura3
     WS.verifyMatch(name2, getPackages.package.coverages[2].name, false, FailureHandling.CONTINUE_ON_FAILURE)
 
-    WS.verifyMatch(banorteCode2, getPackages.package.coverages[2].banorteCode, false, FailureHandling.CONTINUE_ON_FAILURE)
+	WS.verifyMatch(isOptional2, getPackages.package.coverages[2].isOptional.toString(), false, FailureHandling.CONTINUE_ON_FAILURE)
+	
+	WS.verifyMatch(banorteCode2, getPackages.package.coverages[2].banorteCode, false, FailureHandling.CONTINUE_ON_FAILURE)
 
     CustomKeywords.'packages.ValidarMontosNumericos.validarMonto'(price2, getPackages.package.coverages[2].price)
 
     CustomKeywords.'packages.ValidarMontosNumericos.validarMonto'(insuredAmount2, getPackages.package.coverages[2].insuredAmount)
-
+	
+	//Cobertura4
     WS.verifyMatch(name3, getPackages.package.coverages[3].name, false, FailureHandling.CONTINUE_ON_FAILURE)
 
-    WS.verifyMatch(banorteCode3, getPackages.package.coverages[3].banorteCode, false, FailureHandling.CONTINUE_ON_FAILURE)
+	WS.verifyMatch(isOptional3, getPackages.package.coverages[3].isOptional.toString(), false, FailureHandling.CONTINUE_ON_FAILURE)
+	
+	WS.verifyMatch(banorteCode3, getPackages.package.coverages[3].banorteCode, false, FailureHandling.CONTINUE_ON_FAILURE)
 
     CustomKeywords.'packages.ValidarMontosNumericos.validarMonto'(price3, getPackages.package.coverages[3].price)
 
     CustomKeywords.'packages.ValidarMontosNumericos.validarMonto'(insuredAmount3, getPackages.package.coverages[3].insuredAmount)
-
+	
+	//Cobertura5
     WS.verifyMatch(name4, getPackages.package.coverages[4].name, false, FailureHandling.CONTINUE_ON_FAILURE)
 
-    WS.verifyMatch(banorteCode4, getPackages.package.coverages[4].banorteCode, false, FailureHandling.CONTINUE_ON_FAILURE)
+	WS.verifyMatch(isOptional4, getPackages.package.coverages[4].isOptional.toString(), false, FailureHandling.CONTINUE_ON_FAILURE)
+	
+	WS.verifyMatch(banorteCode4, getPackages.package.coverages[4].banorteCode, false, FailureHandling.CONTINUE_ON_FAILURE)
 
     CustomKeywords.'packages.ValidarMontosNumericos.validarMonto'(price4, getPackages.package.coverages[4].price)
 
     CustomKeywords.'packages.ValidarMontosNumericos.validarMonto'(insuredAmount4, getPackages.package.coverages[4].insuredAmount)
-
+	//Cobertura6
     WS.verifyMatch(name5, getPackages.package.coverages[5].name, false, FailureHandling.CONTINUE_ON_FAILURE)
 
-    WS.verifyMatch(banorteCode5, getPackages.package.coverages[5].banorteCode, false, FailureHandling.CONTINUE_ON_FAILURE)
+	WS.verifyMatch(isOptional5, getPackages.package.coverages[5].isOptional.toString(), false, FailureHandling.CONTINUE_ON_FAILURE)
+	
+	WS.verifyMatch(banorteCode5, getPackages.package.coverages[5].banorteCode, false, FailureHandling.CONTINUE_ON_FAILURE)
 
     CustomKeywords.'packages.ValidarMontosNumericos.validarMonto'(price5, getPackages.package.coverages[5].price)
 
     WS.verifyMatch(insuredAmount5, getPackages.package.coverages[5].insuredAmount, false, FailureHandling.CONTINUE_ON_FAILURE)
-
+	
+	//Cobertura7
     WS.verifyMatch(name6, getPackages.package.coverages[6].name, false, FailureHandling.CONTINUE_ON_FAILURE)
 
-    WS.verifyMatch(banorteCode6, getPackages.package.coverages[6].banorteCode, false, FailureHandling.CONTINUE_ON_FAILURE)
+	WS.verifyMatch(isOptional6, getPackages.package.coverages[6].isOptional.toString(), false, FailureHandling.CONTINUE_ON_FAILURE)
+	
+	WS.verifyMatch(banorteCode6, getPackages.package.coverages[6].banorteCode, false, FailureHandling.CONTINUE_ON_FAILURE)
 
     CustomKeywords.'packages.ValidarMontosNumericos.validarMonto'(price6, getPackages.package.coverages[6].price)
 
     WS.verifyMatch(insuredAmount6, getPackages.package.coverages[6].insuredAmount, false, FailureHandling.CONTINUE_ON_FAILURE)
-
+	
+	//Cobertura8
     WS.verifyMatch(name7, getPackages.package.coverages[7].name, false, FailureHandling.CONTINUE_ON_FAILURE)
 
-    WS.verifyMatch(banorteCode7, getPackages.package.coverages[7].banorteCode, false, FailureHandling.CONTINUE_ON_FAILURE)
+	WS.verifyMatch(isOptional7, getPackages.package.coverages[7].isOptional.toString(), false, FailureHandling.CONTINUE_ON_FAILURE)
+	
+	WS.verifyMatch(banorteCode7, getPackages.package.coverages[7].banorteCode, false, FailureHandling.CONTINUE_ON_FAILURE)
 
     CustomKeywords.'packages.ValidarMontosNumericos.validarMonto'(price7, getPackages.package.coverages[7].price)
 
